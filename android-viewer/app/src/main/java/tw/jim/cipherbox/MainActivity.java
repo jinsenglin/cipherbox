024cc51b8abcae0dfa4d530e00bac876c7278474
Jim Lin <jimlintw922@gmail.com>
2015-10-25 11:05:10 +0800

    added ui layout

--- android-viewer/app/src/main/java/tw/jim/cipherbox/MainActivity.java
+++ android-viewer/app/src/main/java/tw/jim/cipherbox/MainActivity.java
@@ -2,12 +2,12 @@ package tw.jim.cipherbox;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null).show();*/
+
+                TextView plaintext = (TextView) findViewById(R.id.textView4);
+                plaintext.setText("This is hello.");
             }
         });
     }
