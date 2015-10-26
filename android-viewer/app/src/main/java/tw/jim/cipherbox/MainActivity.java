f71fa39e8818e15b47fee182fbfbb495ccba7a12
Jim Lin <jimlintw922@gmail.com>
2015-10-26 21:44:13 +0800

    added querying/searching metadata and rsa-key-pairs.tgz files on Google Drive

--- android-viewer/app/src/main/java/tw/jim/cipherbox/MainActivity.java
+++ android-viewer/app/src/main/java/tw/jim/cipherbox/MainActivity.java
@@ -14,7 +14,12 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
 
 import java.io.File;
 
@@ -24,6 +29,18 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     private static final String TAG = "cipherbox";
     private GoogleApiClient mGoogleApiClient;
 
+    final private ResultCallback<DriveApi.MetadataBufferResult> metadataCallback =
+            new ResultCallback<DriveApi.MetadataBufferResult>() {
+                @Override
+                public void onResult(DriveApi.MetadataBufferResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i(TAG, "Problem while retrieving results.");
+                        return;
+                    }
+                    Log.i(TAG, "result: " + result.getMetadataBuffer().get(0).getTitle());
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +49,15 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         setSupportActionBar(toolbar);
 
         // Create application home ".cipherbox" if doesn't exist, using internal storage
-        File file = new File(this.getApplicationContext().getFilesDir(), ".cipherbox");
-        if (file.exists()) {
+        File apphome = new File(this.getApplicationContext().getFilesDir(), ".cipherbox");
+        if (apphome.exists()) {
             Log.i(TAG, "Application home .cipherbox exists.");
         }
         else {
             Log.i(TAG, "Application home .cipherbox does not exist.");
 
             Log.i(TAG, "Trying to create application home .cipherbox ...");
-            if (file.mkdir()) {
+            if (apphome.mkdir()) {
                 Log.i(TAG, "Application home .cipherbox is created.");
             }
             else {
@@ -57,6 +74,8 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                         .setAction("Action", null).show();*/
 
                 // Decrypt cipher text and then display plain text
+                // TODO get InputStream of the specified file from Google Drive
+                // TODO decrypt the content of InputStream using the specified RSA key
                 TextView plaintext = (TextView) findViewById(R.id.textView4);
                 plaintext.setText("This is hello.");
 
@@ -116,6 +135,44 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     @Override
     public void onConnected(Bundle bundle) {
         Log.i(TAG, "API client connected.");
+
+        // Download cipherbox/rsa-key-pairs.tgz to local application home ".cipherbox" if does not exist
+        File apphome = new File(this.getApplicationContext().getFilesDir(), ".cipherbox");
+        File tgzfile = new File(apphome, "rsa-key-pairs.tgz");
+        if (tgzfile.exists()) {
+            Log.i(TAG, "File .cipherbox/rsa-key-pairs.tgz exists.");
+        }
+        else {
+            Log.i(TAG, "File .cipherbox/rsa-key-pairs.tgz does not exist.");
+
+            Log.i(TAG, "Trying to download file rsa-key-pairs.tgz from Google Drive.");
+            Query query = new Query.Builder()
+                    .addFilter(Filters.contains(SearchableField.TITLE, "rsa-key-pairs.tgz"))
+                    .build();
+            Drive.DriveApi.query(mGoogleApiClient, query)
+                    .setResultCallback(metadataCallback);
+
+            // TODO download file rsa-key-pairs.tgz from Google Drive
+            // TODO untar file rsa-key-pairs.tgz
+        }
+
+        // Download cipherbox/metadata to local application home ".cipherbox" if does not exist
+        File metadatafile = new File(apphome, "metadata");
+        if (metadatafile.exists()) {
+            Log.i(TAG, "File .cipherbox/metadata exists.");
+        }
+        else {
+            Log.i(TAG, "File .cipherbox/metadata does not exist.");
+
+            Log.i(TAG, "Trying to download file metadata from Google Drive.");
+            Query query = new Query.Builder()
+                    .addFilter(Filters.contains(SearchableField.TITLE, "metadata"))
+                    .build();
+            Drive.DriveApi.query(mGoogleApiClient, query)
+                    .setResultCallback(metadataCallback);
+
+            // TODO download file metadata from Google Drive
+        }
     }
 
     @Override
